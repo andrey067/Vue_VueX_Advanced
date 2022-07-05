f0054689893d7c016547b7ed73c50c5444e3c08c
andrey067 <audrey.ernesto.lima@gmail.com>
2022-07-04 22:54:05 -0400

    finalizando modulo avançado de Vue.js

--- Vue CLI/projeto-vue-padrao/src/components/HelloWorld.vue
+++ Vue CLI/projeto-vue-padrao/src/components/HelloWorld.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h2>Variavel de ambiente - {{ varialve }}</h2>
+    <h2>Links da API- {{ url_api }}</h2>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
+  data: () => ({
+    varialve: process.env.VUE_APP_MINHA_VARIAVEL_DE_AMBIENTE,
+    url_api: process.env.VUE_APP_API_URL
+  }),
   props: {
     msg: String
   }
@@ -15,21 +21,4 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
